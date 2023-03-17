0ef40e2f13c5b077047f7629cf41130f7e7e46c3
abd-elmoughit <abdelmoughitbeilla20@gmail.com>
2023-03-17 16:21:22 -0700

    task 01-1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,3 +20,11 @@ int n, l;
 	}
 	else if (l == 0)
 	{
+	printf("Last digit of %d is %d and is 0\n", n, l);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
+	return (0);
+}
