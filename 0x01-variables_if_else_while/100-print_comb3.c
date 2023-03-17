cabba0ad400de3c5c6f620e83585b64af3667d13
abd-elmoughit <abdelmoughitbeilla20@gmail.com>
2023-03-17 16:59:54 -0700

    task 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+
+{
+	int d, p;
+
+	for (d = 0; d <= 8; d++) {
+            for (p = d + 1; p <= 9; p++) {
+                 putchar(d + '0');
+	         putchar(p + '0');
+                 if (d < 8) {
+         	    putchar(',');
+	            putchar(' ');
+	         }
+	     }
+	}
+	putchar('\n');
+	return (0);
+}
