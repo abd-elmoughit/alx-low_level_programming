a47a56c3d3bc26ff5b494fcc23b7b94eea449de7
abd-elmoughit <abdelmoughitbeilla20@gmail.com>
2023-03-21 18:04:06 -0700

    task 00-2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,19 @@
 #include "main.h"
 
 /**
- * main - Prints alx  as a message.
+ * main - Entry point
  *
- * Return: Always 0 
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
 	_putchar('\n');
 	return (0);
 }
