d96051a5c682e6db8d45c3c97a235a02bf077b62
abd-elmoughit <abdelmoughitbeilla20@gmail.com>
2023-03-21 19:22:34 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,35 +6,40 @@
  * @n: The value of the times table to be printed.
  */
 
-void print_times_tables(int n)
+void print_times_table(int n)
 {
-	int num, mult, prod;
+	int a, b, op;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (num = 0;num <= n; num++)
+		for (a = 0; a <= n; a++)
 		{
-			_putchar('0');
-			for(mult = 1; mult <= n; mult++)
+			_putchar(48);
+			for (b = 1; b <= n; b++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				prod = num * mult;
-				if (prod <= 99)
+				op = a * b;
+				_putchar(44);
+				_putchar(32);
+				if (op <= 9)
 				{
-					_putchar(' ');
-					if (prod >= 9)
-					{
-						_putchar((prod / 100) + '0');
-						_putchar((prod / 10) %( 10 + '0'));
-					}
-					else if (prod <= 99 && prod >= 10)
-					{
-						_putchar((prod / 10) + '0');
-					}
-					_putchar((prod % 10) + '0');
+					_putchar(32);
+					_putchar(32);
+					_putchar(op + 48);
+				}
+				else if (op <= 99)
+				{
+					_putchar(32);
+					_putchar((op / 10) + 48);
+					_putchar((op % 10) + 48);
+				}
+				else
+				{
+					_putchar(((op / 100) % 10) + 48);
+					_putchar(((op / 10) % 10) + 48);
+					_putchar((op % 10) + 48);
 				}
 			}
+			_putchar('\n');
 		}
 	}
 }
