23eff1ae05dc9b08e17a4565895cdc025bac3764
abd-elmoughit <abdelmoughitbeilla20@gmail.com>
2023-06-14 18:58:28 -0700

    task final edit 00

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,54 +1,51 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
+
 /**
- * main - prints the min number
- * @argc: num of args
- * @argv: string
- * Return: 0 on success, 1 on error
+ * main - entry point
+ * @argc: character
+ * @argv: character
+ *
+ * Description: -
+ * Return: -
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int cents, coins = 0;
+	int coin;
+	int total = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+	int i = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi(argv[1]);
-
-	if (cents < 0)
+	for (i = 1; i < (int)strlen(argv[1]); i++)
 	{
-		printf("0\n");
-		return (0);
+		if (!isdigit(argv[1][i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
 	}
-	while (cents > 0)
+	coin = atoi(argv[1]);
+	while (coin > 0)
 	{
-		if (cents >= 25)
+		i = 0;
+		while (i < 5)
 		{
-			cents -= 25;
+			if (coins[i] <= coin)
+			{
+				coin -= coins[i];
+				total++;
+				break;
+			}
+			i++;
 		}
-		else if (cents >= 10)
-		{
-			cents -= 10;
-		}
-		else if (cents >= 5)
-		{
-			cents -= 5;
-		}
-		else if (cents >= 2)
-		{
-			cents -= 2;
-		}
-		else
-		{
-			cents -= 1;
-		}
-
-		coins++;
 	}
-	printf("%d\n", coins);
+	printf("%d\n", total);
 	return (0);
 }
