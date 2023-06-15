e74f3357afef6b719ed48aa800f0aac8b7d50da8
abd-elmoughit <abdelmoughitbeilla20@gmail.com>
2023-06-14 17:36:32 -0700

    task 12001

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,11 +1,16 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
+/**
+ * _isupper - entry point
+ * @c: character
+ *
+ * Description: checks for uppercase character
+ * Return: 1 if c is uppercase 0 otherwise
+ */
 int _isupper(int c)
 {
-    return (c >= 'A' && c <= 'Z');
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
-
--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,11 +1,27 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
+/**
+ * _strcat - function that concatenates two strings
+ * @src: source string
+ * @dest: destination string
+ * Return: pointer to the resulting string dest
+ */
 char *_strcat(char *dest, char *src)
 {
-    return strcat(dest, src);
-}
+	int i;
+	int j;
 
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		if (dest[i + 1] == '\0')
+		{
+			for (j = 0; src[j] != '\0'; j++)
+			{
+				dest[i + 1] = src[j];
+				i++;
+			}
+		}
+	}
+
+	return (dest);
+}
