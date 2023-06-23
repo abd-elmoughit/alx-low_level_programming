d42bd241c2c767602fc5bd75d65c8aa7bd9b23b8
abd-elmoughit <abdelmoughitbeilla20@gmail.com>
2023-06-22 17:05:39 -0700

    task 001

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,29 +1,27 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-
 
 /**
-* sum_them_all - Returns the sum of all its paramters.
-* @n: The number of paramters passed to the function.
-* @...: A variable number of paramters to calculate the sum of.
-*
-* Return: If n == 0 - 0.
-*         Otherwise - the sum of all parameters.
-*/
+ * sum_them_all - entry point
+ * @n: size of triangle
+ * Description: --
+ * Return: --
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-va_list ap;
-unsigned int i, sum = 0;
+	va_list args;
+	int sum = 0, i = 0;
 
+	va_start(args, n);
 
-va_start(ap, n);
+	if (n == 0)
+		return (0);
 
+	while (i < (int)n)
+	{
+		sum += va_arg(args, int);
+		i++;
+	}
+	va_end(args);
 
-for (i = 0; i < n; i++)
-sum += va_arg(ap, int);
-
-va_end(ap);
-
-
-return (sum);
+	return (sum);
 }
